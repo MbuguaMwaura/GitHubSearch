6c0bfa1fdc3b5610db775d30b962d83a8e573452
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-04 20:48:13 +0300

    imports all the dependencies into the user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
